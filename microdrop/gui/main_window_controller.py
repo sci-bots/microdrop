a33cb8b87ebc7a46ca95353a5aae32150a8254f6
Ryan Fobel <ryan@fobel.net>
2011-10-21 11:45:44 -0400

    Don't throw exception if no device is connected

--- microdrop/gui/main_window_controller.py
+++ microdrop/gui/main_window_controller.py
@@ -81,26 +81,24 @@ class MainWindowController:
                 except:
                     pass
 
-
-        firmware_version = self.app.control_board.software_version()
-        driver_version = self.app.control_board.host_software_version()
-        del self.app.control_board
-
-        try:
-            f = FirmwareUpdater()
-            updated = f.update(self.port,
-                        firmware_version=firmware_version,
-                        driver_version=driver_version)
-            if updated:
-                self.error('Driver/Firmware was updated.  Application must be restarted.')
-                sys.exit(0)
-        except FirmwareError:
-            pass
-
-        self.app.control_board = DmfControlBoard()
-
-        self._register_serial_device(self.port)
-
+        if self.app.control_board.connected():
+            firmware_version = self.app.control_board.software_version()
+            driver_version = self.app.control_board.host_software_version()
+            del self.app.control_board
+    
+            try:
+                f = FirmwareUpdater()
+                updated = f.update(self.port,
+                            firmware_version=firmware_version,
+                            driver_version=driver_version)
+                if updated:
+                    self.error('Driver/Firmware was updated.  Application must be restarted.')
+                    sys.exit(0)
+            except FirmwareError:
+                pass
+    
+            self.app.control_board = DmfControlBoard()
+            self._register_serial_device(self.port)
 
     def _register_serial_device(self, port):
         if self.app.control_board.connect(port) == DmfControlBoard.RETURN_OK:
