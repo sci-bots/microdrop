b821336da185c10bcbe24240ef4d7d00ed363472
Christian Fobel <christian@fobel.net>
2011-11-10 20:51:32 -0500

    When selecting a new devices directory path, ...
    When selecting a new devices directory path, if target path is not empty,
    prompt to ask whether directories should be merged.  Note that any paths
    already existing in the target directory will be overwritten.

--- microdrop/gui/options_controller.py
+++ microdrop/gui/options_controller.py
@@ -24,6 +24,60 @@ import gtk
 from path import path
 
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    import os
+    from shutil import copy2, copystat, Error
+
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+
+    try:
+        os.makedirs(dst)
+    except OSError, exc:
+        # XXX - this is pretty ugly
+        if "file already exists" in exc[1]:  # Windows
+            pass
+        elif "File exists" in exc[1]:        # Linux
+            pass
+        else:
+            raise
+
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                copy2(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error), why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except Error, err:
+            errors.extend(err.args[0])
+    try:
+        copystat(src, dst)
+    except WindowsError:
+        # can't copy file access times on Windows
+        pass
+    except OSError, why:
+        errors.extend((src, dst, str(why)))
+    if errors:
+        raise Error, errors 
+
+
+
 class OptionsController:
     def __init__(self, app):
         self.app = app
@@ -64,11 +118,15 @@ class OptionsController:
         print 'apply changes:', data_dir
         data_dir.makedirs_p()
         if data_dir.listdir():
-            self.app.main_window_controller.error('''Directory must be empty''')
-            return
+            from standalone_message_dialog import MessageDialog
+
+            m = MessageDialog()
+            result = m.ques('Target directory is not empty.  Merge contents with current devices (overwriting common paths in the target directory)?')
+            if not result == gtk.RESPONSE_OK:
+                return
 
         for d in self.app.config.dmf_device_directory.dirs():
-            d.copytree(data_dir.joinpath(d.name))
+            copytree(d, data_dir.joinpath(d.name))
         for f in self.app.config.dmf_device_directory.files():
             f.copyfile(data_dir.joinpath(f.name))
         self.app.config.dmf_device_directory.rmtree()
--- microdrop/gui/standalone_message_dialog.py
+++ microdrop/gui/standalone_message_dialog.py
@@ -6,30 +6,34 @@ class MessageDialog(gtk.Window):
         md = gtk.MessageDialog(self, 
             gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, 
             gtk.BUTTONS_CLOSE, message)
-        md.run()
+        result = md.run()
         md.destroy()
+        return result
         
     
     def erro(self, message):
         md = gtk.MessageDialog(self, 
             gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, 
             gtk.BUTTONS_CLOSE, message)
-        md.run()
+        result = md.run()
         md.destroy()
+        return result
     
     
     
     def ques(self, message):
         md = gtk.MessageDialog(self, 
             gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION, 
-            gtk.BUTTONS_CLOSE, message)
-        md.run()
+            gtk.BUTTONS_OK_CANCEL, message)
+        result = md.run()
         md.destroy()
+        return result
     
     
     def warn(self, message):
         md = gtk.MessageDialog(self, 
             gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_WARNING, 
             gtk.BUTTONS_CLOSE, message)
-        md.run()
+        result = md.run()
         md.destroy()
+        return result
