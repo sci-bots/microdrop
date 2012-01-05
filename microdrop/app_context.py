e35bcf759834a60c9e7b859686ade49073c3dae2
Christian Fobel <christian@fobel.net>
2012-01-05 10:39:22 -0500

    Add missing file: app_context.py

---
+++ microdrop/app_context.py
@@ -0,0 +1,28 @@
+"""
+Copyright 2011 Ryan Fobel
+
+This file is part of Microdrop.
+
+Microdrop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Microdrop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Microdrop.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from plugin_manager import PluginManager
+
+
+def get_app():
+    service = plugin_manager.enable('App', env='microdrop')
+    return service
+
+
+plugin_manager = PluginManager()
