cd5dbf5e24f453443b591f5e3267f2ff16b3f62f
Ryan Fobel <ryan@fobel.net>
2012-01-24 11:04:36 -0500

    Get experiment log "notes" from the end of the list

--- microdrop/gui/experiment_log_controller.py
+++ microdrop/gui/experiment_log_controller.py
@@ -229,7 +229,7 @@ class ExperimentLogController(SingletonPlugin):
     def on_textview_notes_focus_out_event(self, widget, data=None):
         if len(self.results.log.data[0])==0:
             self.results.log.data.append({})
-        self.results.log.data[0]["notes"] = textview_get_text(self.builder. \
+        self.results.log.data[-1]["notes"] = textview_get_text(self.builder. \
             get_object("textview_notes"))
         filename = os.path.join(self.results.log.directory,
                                 str(self.results.log.experiment_id),
