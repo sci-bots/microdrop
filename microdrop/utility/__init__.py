fd43514167299d695537dd9a5afd966d3243ba80
Ryan Fobel <ryan@fobel.net>
2012-02-08 15:46:10 -0500

    Add Version class

--- microdrop/utility/__init__.py
+++ microdrop/utility/__init__.py
@@ -19,6 +19,7 @@ along with Microdrop.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 import sys
+import re
 
 from path import path
 
@@ -105,4 +106,75 @@ def textview_get_text(textview):
     buffer = textview.get_buffer()
     start = buffer.get_start_iter()
     end = buffer.get_end_iter()
-    return buffer.get_text(start, end)    
+    return buffer.get_text(start, end)
+
+class InvalidVersionString(Exception):
+    pass
+
+class InvalidVersion(Exception):
+    pass
+
+class Version:
+    InvalidVersionString(Exception)
+    
+    def __init__(self, major=0, minor=0, micro=0):
+        if type(major)!=int or type(minor)!=int or type(micro)!=int:
+            raise InvalidVersion
+        self.major = major
+        self.minor = minor
+        self.micro = micro
+
+    @classmethod
+    def fromstring(cls, string):
+        "Initialize Version from a string"
+        major, minor, micro = ('0','0','0')
+        match = False
+        
+        m = re.search('^(\d+)$', string)
+        if m:
+            major = m.groups()[0]
+            match = True
+        m = re.search('^(\d+)\.(\d+)$', string)
+        if m:
+            major, minor = m.groups()
+            match = True
+        m = re.search('^(\d+)\.(\d+)\.(\d+)$', string)
+        if m:
+            major, minor, micro = m.groups()
+            match = True
+            
+        if match == False:
+            raise InvalidVersionString
+
+        return cls(int(major), int(minor), int(micro))
+    
+    def __repr__(self):
+        return "%s(%d.%d.%d)" % (self.__class__,
+                                 self.major,
+                                 self.minor,
+                                 self.micro)
+
+    def __str__(self):
+        return "%d.%d.%d" % (self.major,
+                             self.minor,
+                             self.micro)
+        
+    def __lt__(self, x):
+        if self.major<x.major:
+            return True
+        elif self.major==x.major:
+            if self.minor<x.minor:
+                return True
+            elif self.minor==x.minor:
+                if self.micro<x.micro:
+                    return True
+        return False
+    
+    def __eq__(self, x):
+        if (self.major, self.minor, self.micro) == (x.major, x.minor, x.micro):
+            return True
+        else:
+            return False
+        
+    def __le__(self, x):
+        return self<x or self==x
