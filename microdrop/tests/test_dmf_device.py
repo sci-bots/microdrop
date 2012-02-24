ec316535728215461176347d9c850d8ae34a5576
Ryan Fobel <ryan@fobel.net>
2012-02-24 09:41:20 -0500

    Add tests for loading DMF devices

---
+++ microdrop/tests/test_dmf_device.py
@@ -0,0 +1,37 @@
+from path import path
+from nose.tools import raises
+
+from dmf_device import DmfDevice
+from utility import Version
+
+def test_load_dmf_device():
+    """
+    test loading DMF device files
+    """
+
+    # version 0.2.0 files
+    for i in [0,1]:
+        yield load_device, (path(__file__).parent /
+                            path('devices') /
+                            path('device %d v%s' % (i, Version(0,2,0))))
+
+    # version 0.3.0 files
+    for i in [1]:
+        yield load_device, (path(__file__).parent /
+                            path('devices') /
+                            path('device %d v%s' % (i, Version(0,3,0))))
+
+
+def load_device(name):
+    DmfDevice.load(name)
+    assert True
+
+
+@raises(IOError)
+def test_load_non_existant_dmf_device():
+    """
+    test loading DMF device file that doesn't exist
+    """
+    DmfDevice.load(path(__file__).parent /
+                   path('devices') /
+                   path('no device'))
