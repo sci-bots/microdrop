1de78f7b010b989307ae6f3c88ac4566bf318fb6
Christian Fobel <christian@fobel.net>
2012-05-25 03:45:24 -0400

    Add missing app_sink code.

--- microdrop/gui/play_bin.py
+++ microdrop/gui/play_bin.py
@@ -40,11 +40,23 @@ class PlayBin(gst.Bin):
         rate_caps_filter = gst.element_factory_make('capsfilter', 'rate_caps_filter')
         rate_caps_filter.set_property('caps', rate_caps)
 
-        capture_queue = gst.element_factory_make('queue', 'capture_queue')
+        self.app_sink = gst.element_factory_make('appsink', 'app_sink')
+        self.app_sink.set_property('drop', True)
+        self.app_sink.set_property('max-buffers', 1)
+        app_sink_color_in = gst.element_factory_make('ffmpegcolorspace', 'app_sink_color_in')
+        self.app_sink.set_property('caps',
+                gst.caps_from_string('video/x-raw-rgb,depth=24'))
 
-        self.add(video_src, video_caps_filter, video_rate,
+        app_sink_queue = gst.element_factory_make('queue', 'app_sink_queue')
+        capture_queue = gst.element_factory_make('queue', 'capture_queue')
+        test_queue = gst.element_factory_make('queue', 'test_queue')
+
+        self.add(self.video_caps_filter, video_rate,
                 rate_caps_filter, video_tee,
 
+                app_sink_queue,
+                app_sink_color_in,
+                self.app_sink,
                 feed_queue,
 
                 video_scale, self.scale_caps_filter,
@@ -58,25 +70,30 @@ class PlayBin(gst.Bin):
         if time_overlay:
             self.add(_time_overlay)
 
-        gst.element_link_many(video_src, video_caps_filter, feed_queue)
-        gst.element_link_many(warp_in_color, self.warper, warp_out_color,
-                video_rate, rate_caps_filter,
-                video_tee,
+        gst.element_link_many(self.video_caps_filter, feed_queue)
+        gst.element_link_many(video_tee,
+                warp_in_color, self.warper, warp_out_color,
+                video_rate, rate_caps_filter, 
                 video_scale, self.scale_caps_filter,
                 cairo_color_in, cairo_draw, cairo_color_out,
                 self.text_overlay,
                 video_sink)
 
         if time_overlay:
-            gst.element_link_many(feed_queue, _time_overlay, warp_in_color)
+            gst.element_link_many(feed_queue, _time_overlay, video_tee)
         else:
-            gst.element_link_many(feed_queue, warp_in_color)
+            gst.element_link_many(feed_queue, video_tee)
 
         video_tee.link(capture_queue)
+        gst.element_link_many(video_tee, app_sink_queue, app_sink_color_in,
+                self.app_sink)
 
         # Add ghost 'src' pad 
         self.play_bin_src_gp = gst.GhostPad("src", capture_queue.get_pad('src'))
         self.add_pad(self.play_bin_src_gp)
+
+        self._video_src = None
+        self.video_src = video_src
 
     def scale(self, width=None, height=None):
         if width is None and height is None:
