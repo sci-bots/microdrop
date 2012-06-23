634b53f012b38c48169bc2e5ccf28903f1e2c00f
Christian Fobel <christian@fobel.net>
2012-06-23 15:51:42 -0400

    Add post-install configuration for GStreamer
    Copy correct version of Python binding to system site-packages path.
    Instruct user to add %GST_PLUGIN_PATH% to the system PATH.
    Fixes #108.

--- install_dependencies.py
+++ install_dependencies.py
@@ -20,6 +20,7 @@ along with Microdrop.  If not, see <http://www.gnu.org/licenses/>.
 import urllib2
 import subprocess
 import sys
+import site
 import tempfile
 import os
 import zipfile
@@ -27,24 +28,73 @@ import tarfile
 import re
 from distutils.dir_util import copy_tree
 
+from path import path
+
 
 PYTHON_VERSION = "%s.%s" % (sys.version_info[0],
                             sys.version_info[1])
 CACHE_PATH = "download_cache"
 
-def get_version(path):
+
+def get_version(git_path):
     from site_scons.git_util import GitUtil
 
-    g = GitUtil(path)
+    g = GitUtil(git_path)
     version = g.describe()
     m = re.match('v(\d+)\.(\d+)-(\d+)', version)
     return "%s.%s.%s" % (m.group(1), m.group(2), m.group(3))
+
 
 def in_path(filename):
     for d in os.environ['PATH'].split(';'):
         if os.path.isfile(os.path.join(d, filename)):
             return True
     return False
+
+
+def configure_gstreamer():
+    from path import path
+
+    try:
+        gstreamer_dir = path(os.environ['OSSBUILD_GSTREAMER_DIR'])
+    except KeyError:
+        # GStreamer has not been installed
+        return
+
+    version_str = 'v%s.%s' % (sys.version_info.major, sys.version_info.minor)
+    python_lib_path = path(os.environ['OSSBUILD_GSTREAMER_SDK_DIR']).joinpath(
+            'bindings', 'python', version_str, 'lib')
+    python_package_path = python_lib_path.joinpath('site-packages', 'gst-0.10',
+            'gst')
+
+    site_packages_path = path(site.getsitepackages()[-1]).joinpath(
+            python_package_path.name)
+
+    gst_plugin_path = path(os.environ['GST_PLUGIN_PATH'])
+    py26_dll_path = gst_plugin_path.joinpath('libgstpython-v2.6.dll')
+    py_dll_path = gst_plugin_path.joinpath('libgstpython-%s.dll' % version_str)
+
+    if not py_dll_path.isfile():
+        print '%s not found.  Copy files for Python %s' % (py_dll_path,
+                version_str)
+        copy_tree(python_lib_path, gstreamer_dir.joinpath(python_lib_path.name))
+    if not site_packages_path.isdir():
+        print '%s not found.  Copy files Python site-packages' % (
+                site_packages_path)
+        print 'from %s -> %s' % (python_package_path, site_packages_path)
+        copy_tree(python_package_path, site_packages_path)
+
+    if not (sys.version_info.minor == 2 and sys.version_info.minor == 6)\
+        and py26_dll_path.isfile():
+        py26_dll_path.rename(py26_dll_path + 'x')
+
+    path_entries = set([path(p) for p in os.environ['PATH'].split(';')])
+
+    if gst_plugin_path not in path_entries:
+        return [r'%GST_PLUGIN_PATH% must be added to the PATH environment'\
+                ' variable']
+    return []
+
 
 def install(package):
     name = package[0]
@@ -96,8 +146,8 @@ def install(package):
         
 
 def download_file(link, name, type):
-    path = os.path.join(CACHE_PATH,"%s.%s" % (name,type))
-    fp = open(path, 'wb')
+    filepath_path = os.path.join(CACHE_PATH,"%s.%s" % (name,type))
+    fp = open(filepath_path, 'wb')
     downloaded = 0
     print link
     resp = urllib2.urlopen(link)
@@ -122,7 +172,7 @@ def download_file(link, name, type):
         fp.close()
         sys.stdout.write('\n');
         sys.stdout.flush()
-    return path
+    return filepath_path
 
 if __name__ == "__main__":
     if os.name!='nt':
@@ -175,8 +225,6 @@ if __name__ == "__main__":
               ("pymunk", "exe", "http://pymunk.googlecode.com/files/pymunk-2.1.0.win32.exe"),
               ("pylint", "pip", "http://download.logilab.org/pub/pylint/pylint-0.25.1.tar.gz"),
               ("constraint", "pip", "https://github.com/cfobel/python___labix_constraint/tarball/master"),
-              ("gstreamer", "msi", "http://ossbuild.googlecode.com/files/GStreamer-WinBuilds-LGPL-x86-Beta04-0.10.7.msi"),
-              ("gstreamer-sdk", "msi", "http://ossbuild.googlecode.com/files/GStreamer-WinBuilds-SDK-LGPL-x86-Beta04-0.10.7.msi"),
               ):
         try:
             exec("import " + p[0])
@@ -226,6 +274,19 @@ if __name__ == "__main__":
             packages.append(("pywin32", "exe", "http://sourceforge.net/projects/pywin32/files/pywin32/Build216/pywin32-216.win32-py2.6.exe/download"))
         elif PYTHON_VERSION=="2.7":
             packages.append(("pywin32", "exe", "http://sourceforge.net/projects/pywin32/files/pywin32/Build216/pywin32-216.win32-py2.7.exe/download"))
+
+
+    # check if pywin32 is installed
+    try:
+        exec("import gst")
+    except:
+        packages.append(("gst", "msi",
+                'http://ossbuild.googlecode.com/files/'\
+                        'GStreamer-WinBuilds-LGPL-x86-Beta04-0.10.7.msi'))
+        packages.append(("gst", "msi",
+                'http://ossbuild.googlecode.com/files/'\
+                        'GStreamer-WinBuilds-SDK-LGPL-x86-Beta04-0.10.7.msi'))
+
 
     # check if pyinstaller is installed
     if os.path.isdir("C:/pyinstaller")==False:
@@ -304,6 +365,8 @@ if __name__ == "__main__":
                 warnings.append("Warning: %s exists but you need to "\
                       "add it to your path." % d)
                 break
+
+    warnings += configure_gstreamer()    
     
     print "\nAll dependencies are installed."
 
