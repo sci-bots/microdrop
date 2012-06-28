e8733886665323af7d498b77e1ac74dadf5b714b
Ryan Fobel <ryan@fobel.net>
2012-06-28 16:06:35 -0400

    Fix dmf_control_board update script

--- microdrop/tests/update_dmf_control_board.py
+++ microdrop/tests/update_dmf_control_board.py
@@ -1,13 +1,20 @@
 import os
+import sys
 import subprocess
 
 if __name__ == '__main__':
     os.chdir('microdrop/plugins')
-    
-    if not os.path.exists('dmf_control_board'):
-        print 'Clone dmf_control_board repository...'
-        subprocess.check_call(['git', 'clone',
-            'http://microfluidics.utoronto.ca/git/dmf_control_board.git'])
-    else:
-        print 'Fetch lastest update...'
-        subprocess.check_call(['git', 'pull'])
+    try:
+        if not os.path.exists('dmf_control_board'):
+            print 'Clone dmf_control_board repository...'
+            print subprocess.check_output(['git', 'clone',
+                'http://microfluidics.utoronto.ca/git/dmf_control_board.git'],
+                stderr=subprocess.STDOUT)
+        else:
+            print 'Fetch lastest dmf_control_board update...'
+            print subprocess.check_output(['git', 'pull'],
+                                          stderr=subprocess.STDOUT)
+        sys.exit(0)
+    except subprocess.CalledProcessError, e:
+        print e.output
+        sys.exit(e.returncode)
