66da5a6bd67ae3645eeff5856ae4614e4be9f5d8
Ryan Fobel <ryan@fobel.net>
2012-06-28 15:00:01 -0400

    Add script for downloading latest dmf_control_board

---
+++ microdrop/tests/update_dmf_control_board.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+if __name__ == '__main__':
+    os.chdir('microdrop/plugins')
+    
+    if not os.path.exists('dmf_control_board'):
+        print 'Clone dmf_control_board repository...'
+        subprocess.call(['git', 'clone',
+            'http://microfluidics.utoronto.ca/git/dmf_control_board.git'])
+    else:
+        print 'Fetch lastest update...'
+        subprocess.call(['git', 'pull'])
