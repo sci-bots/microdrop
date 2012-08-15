84fc319af5b6ac65fe58ed4049149696135bb3b4
Ryan Fobel <ryan@fobel.net>
2012-08-15 18:36:20 -0400

    Fix loading plugins from non-default directory

--- microdrop/plugin_manager.py
+++ microdrop/plugin_manager.py
@@ -306,18 +306,18 @@ def load_plugins(plugins_dir='plugins'):
     if plugins_dir.parent.abspath() not in sys.path:
         sys.path.insert(0, plugins_dir.parent.abspath())
     
-        for d in plugins_dir.dirs():
-            package = (d / path('microdrop'))
-            if package.isdir(): 
-                try:
-                    logging.info('\t %s' % package.abspath())
-                    import_statement = 'import %s.%s.microdrop' % \
-                        (plugins_dir.name, d.name)
-                    logging.debug(import_statement)
-                    exec(import_statement)
-                except Exception, why:
-                    logging.debug(''.join(traceback.format_exc()))
-                    logging.error('Error loading %s plugin.' % d.name)
+    for d in plugins_dir.dirs():
+        package = (d / path('microdrop'))
+        if package.isdir(): 
+            try:
+                logging.info('\t %s' % package.abspath())
+                import_statement = 'import %s.%s.microdrop' % \
+                    (plugins_dir.name, d.name)
+                logging.debug(import_statement)
+                exec(import_statement)
+            except Exception, why:
+                logging.debug(''.join(traceback.format_exc()))
+                logging.error('Error loading %s plugin.' % d.name)
 
     # Create an instance of each of the plugins, but set it to disabled
     e = PluginGlobals.env('microdrop.managed')
