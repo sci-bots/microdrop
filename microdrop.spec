sci-bots/microdrop

commit f641d7b3e8c4f568ab98f27ce47f3188bdb1d444

Add GStreamer video source caps query module
Add GStreamer video source caps query module to install_dependencies.py

microdrop.spec
@@ -16,12 +16,13 @@ extra_py = []
 a = Analysis([os.path.join(HOMEPATH,'support\\_mountzlib.py'),
             os.path.join(HOMEPATH,'support\\useUnicode.py'),
             'microdrop\\microdrop.py'] + extra_py,
-            excludes=['opencv', 'flatland',])
+            excludes=['opencv', 'flatland', 'pygtkhelpers',])
 
 for mod in [pygtkhelpers]:
-        mod_path = path(mod.__file__).parent
-        a.datas += [(str(mod_path.parent.relpathto(p)), str(p.abspath()), 'DATA')\
-                    for p in mod_path.walkfiles(ignore=[r'\.git', r'site_scons', r'.*\.pyc'])]
+	mod_path = path(mod.__file__).parent
+	a.datas += [(str(mod_path.parent.relpathto(p)), str(p.abspath()), 'DATA')\
+		    for p in mod_path.walkfiles(ignore=[r'\.git', r'site_scons',
+                    r'.*\.pyc'])]
 
 # Copy matplotlib mpl-data files to dist directory.
 matplotlib_path = path(matplotlib.__file__).parent

install_dependencies.py
@@ -180,7 +180,9 @@ if __name__ == "__main__":
     # package name, type, url
     for p in (
             ("pygtkhelpers", "pip", "https://github.com/cfobel/pygtkhelpers/"\
-                    "tarball/pre_object_tree"), ):
+                    "tarball/pre_object_tree"), 
+            ('gst_video_source_caps_query', 'pip', 'https://github.com/cfobel/'\
+                    'python___gst_video_source_caps_query/tarball/master'), ):
         packages.append(p)
 
     # python 2.7 specific packages

commit bc8b8f18862256143d34270884061aa535fcbb9f

Fix microdrop.spec file (broken in commit f641d7b3)

microdrop.spec
@@ -16,13 +16,12 @@ extra_py = []
 a = Analysis([os.path.join(HOMEPATH,'support\\_mountzlib.py'),
             os.path.join(HOMEPATH,'support\\useUnicode.py'),
             'microdrop\\microdrop.py'] + extra_py,
-            excludes=['opencv', 'flatland', 'pygtkhelpers',])
+            excludes=['opencv', 'flatland',])
 
 for mod in [pygtkhelpers]:
-	mod_path = path(mod.__file__).parent
-	a.datas += [(str(mod_path.parent.relpathto(p)), str(p.abspath()), 'DATA')\
-		    for p in mod_path.walkfiles(ignore=[r'\.git', r'site_scons',
-                    r'.*\.pyc'])]
+        mod_path = path(mod.__file__).parent
+        a.datas += [(str(mod_path.parent.relpathto(p)), str(p.abspath()), 'DATA')\
+                    for p in mod_path.walkfiles(ignore=[r'\.git', r'site_scons', r'.*\.pyc'])]
 
 # Copy matplotlib mpl-data files to dist directory.
 matplotlib_path = path(matplotlib.__file__).parent