96d33217f0922e4a676d4d5b51b53516795ee010
Christian Fobel <christian@fobel.net>
2014-02-10 16:04:29 -0500

    Add auto git-describe version for Python packag...
    Add automatic Python package versioning using `git-describe` through the
    `version.py` module from [here][1].
    
    A Python package source distribution can be built using the following
    command:
    
        python setup.py sdist
    
    __NB__ The version strings generated by `version.getVersion()` conform
    to [PEP386][2].
    
    [1]: https://gist.github.com/mina86/8782771
    [2]: http://www.python.org/dev/peps/pep-0386

---
+++ README.md
@@ -0,0 +1,17 @@
+MicroDrop
+=========
+
+Microdrop is a graphical user interface for the [DropBot][1] digital
+microfluidics control system (described in detail in [Fobel et al., Appl. Phys.
+Lett. 102, 193513 (2013)][2]). If you use this information in work that you
+publish, please cite as appropriate.
+
+[1]: http://microfluidics.utoronto.ca/microdrop
+[2]: http://dx.doi.org/10.1063/1.4807118
+
+
+Credits
+=======
+
+Ryan Fobel <ryan@fobel.net>
+Christian Fobel <christian@fobel.net>
--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from distutils.core import setup
 
+import version
+
 
 setup(name='microdrop',
-      version='0.0.1',
+      version=version.getVersion(),
       description='Microdrop is a graphical user interface for the DropBot '
                   'Digital Microfluidics control system',
       keywords='digital microfluidics dmf automation dropbot microdrop',
       author='Ryan Fobel and Christian Fobel',
+      author_email='ryan@fobel.net, christian@fobel.net',
       url='http://microfluidics.utoronto.ca/microdrop',
       license='GPL',
-      long_description='''
-Microdrop is a graphical user interface for the [DropBot][1] digital
-microfluidics control system (described in detail in [Fobel et al., Appl.
-Phys. Lett. 102, 193513 (2013)][2]). If you use this information in work that
-you publish, please cite as appropriate.
-
-[1]: http://microfluidics.utoronto.ca/microdrop
-[2]: http://dx.doi.org/10.1063/1.4807118'''.strip(),
+      long_description='\n%s\n' % open('README.md', 'rt').read(),
       packages=['microdrop',
                 'microdrop.gui',
                 'microdrop.tests'],
