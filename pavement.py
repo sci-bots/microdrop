40da05810c2cbf832c5f0da711fa9c3fbfdbd761
Christian Fobel <christian@fobel.net>
2015-01-05 16:25:23 -0500

    Fix #154, create requirements.txt on sdist creation

---
+++ pavement.py
@@ -0,0 +1,53 @@
+import os
+import pkg_resources
+
+from paver.easy import task, needs
+from paver.setuputils import setup
+
+import version
+
+
+install_requires = ['wheeler.pygtkhelpers', 'blinker', 'path-helpers',
+                    'ipython', 'pyutilib==3.9.2706', 'pyparsing', 'configobj',
+                    'pyyaml', 'pyzmq', 'opencv-helpers', 'pygst-utils',
+                    'geo-util', 'flatland-fork', 'microdrop_utility',
+                    'svg_model', 'task_scheduler', 'application_repository',
+                    'pygtk_textbuffer_with_undo']
+
+
+setup(name='microdrop',
+      version=version.getVersion(),
+      description='Microdrop is a graphical user interface for the DropBot '
+                  'Digital Microfluidics control system',
+      keywords='digital microfluidics dmf automation dropbot microdrop',
+      author='Ryan Fobel and Christian Fobel',
+      author_email='ryan@fobel.net, christian@fobel.net',
+      url='http://microfluidics.utoronto.ca/microdrop',
+      license='GPL',
+      long_description='\n%s\n' % open('README.md', 'rt').read(),
+      packages=['microdrop',
+                'microdrop.gui',
+                'microdrop.tests'],
+      package_data={'microdrop.tests': ['devices/*', 'experiment_logs/*',
+                                        'protocols/*', 'svg_files/*.svg',
+                                        'buildbot/master.cfg'],
+                    'microdrop.gui': ['glade/*.glade'],
+                    'microdrop': ['requirements.txt']},
+      install_requires=install_requires)
+
+
+@task
+def create_requirements():
+    package_list = [p.split('==')[0] for p in install_requires]
+    requirements_path = os.path.join('microdrop', 'requirements.txt')
+    with open(requirements_path, 'wb') as output:
+        output.write('\n'.join(['%s==%s' %
+                                (p, pkg_resources.get_distribution(p).version)
+                                for p in package_list]))
+
+@task
+@needs('generate_setup', 'minilib', 'create_requirements',
+       'setuptools.command.sdist')
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
