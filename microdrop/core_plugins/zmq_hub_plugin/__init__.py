e75408e3a59ca4bf26d9d413a93e8a665bd3d211
Christian Fobel <christian@fobel.net>
2015-12-14 08:27:19 -0500

    Fork to create monitor_plugin

--- microdrop/core_plugins/zmq_hub_plugin/__init__.py
+++ microdrop/core_plugins/zmq_hub_plugin/__init__.py
@@ -1,9 +1,9 @@
 """
 Copyright 2015 Christian Fobel
 
-This file is part of droplet_planning_plugin.
+This file is part of monitor_plugin.
 
-droplet_planning_plugin is free software: you can redistribute it and/or modify
+monitor_plugin is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -14,27 +14,22 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with droplet_planning_plugin.  If not, see <http://www.gnu.org/licenses/>.
+along with monitor_plugin.  If not, see <http://www.gnu.org/licenses/>.
 """
-import sys, traceback
-from datetime import datetime
-from collections import OrderedDict
-
+import zmq
 from path_helpers import path
-from flatland import Integer, Boolean, Form, String
-from flatland.validation import ValueAtLeast, ValueAtMost
+from flatland import Form, String
 from microdrop.logger import logger
 from microdrop.plugin_helpers import (AppDataController, StepOptionsController,
                                       get_plugin_info)
 from microdrop.plugin_manager import (PluginGlobals, Plugin, IPlugin,
                                       implements, emit_signal)
 from microdrop.app_context import get_app
-import gobject
-import gtk
 
 PluginGlobals.push_env('microdrop.managed')
 
-class DropletPlanningPlugin(Plugin, AppDataController, StepOptionsController):
+
+class MonitorPlugin(Plugin, AppDataController):
     """
     This class is automatically registered with the PluginManager.
     """
@@ -58,33 +53,196 @@ class DropletPlanningPlugin(Plugin, AppDataController, StepOptionsController):
             config file, in a section named after this plugin's name attribute
     '''
     AppFields = Form.of(
-        Integer.named('transition_duration_ms').using(optional=True,
-                                                      default=750),
-    )
-
-    '''
-    StepFields
-    ---------
-
-    A flatland Form specifying the per step options for the current plugin.
-    Note that nested Form objects are not supported.
-
-    Since we subclassed StepOptionsController, an API is available to access and
-    modify these attributes.  This API also provides some nice features
-    automatically:
-        -all fields listed here will be included in the protocol grid view
-            (unless properties=dict(show_in_gui=False) is used)
-        -the values of these fields will be stored persistently for each step
-    '''
-    StepFields = Form.of(
-        Integer.named('min_duration').using(default=0, optional=True),
+        String.named('publish_uri').using(optional=True, default=''),
     )
 
     def __init__(self):
         self.name = self.plugin_name
-        self.timeout_id = None
-        self.start_time = None
-        self.transition_counter = 0
+        self.context = None
+        self.pub_socket = None
+
+    def verify_connected(self):
+        if self.context is None:
+            app_values = self.get_app_values()
+            if 'publish_uri' in app_values and app_values['publish_uri']:
+                self.context = zmq.Context.instance()
+                self.pub_socket = zmq.Socket(self.context, zmq.PUB)
+                self.pub_socket.bind(app_values['publish_uri'])
+            else:
+                return False
+        return True
+
+    def get_schedule_requests(self, function_name):
+        """
+        Returns a list of scheduling requests (i.e., ScheduleRequest
+        instances) for the function specified by function_name.
+        """
+        return []
+
+    def on_plugin_disable(self):
+        """
+        Handler called once the plugin instance is disabled.
+        """
+        if self.context is not None:
+            self.pub_socket.close()
+            del self.context
+
+    def on_plugin_enable(self):
+        """
+        Handler called once the plugin instance is enabled.
+
+        Note: if you inherit your plugin from AppDataController and don't
+        implement this handler, by default, it will automatically load all
+        app options from the config file. If you decide to overide the
+        default handler, you should call:
+
+            AppDataController.on_plugin_enable(self)
+
+        to retain this functionality.
+        """
+        self.verify_connected()
+        super(MonitorPlugin, self).on_plugin_enable()
+
+    def on_app_exit(self):
+        """
+        Handler called just before the Microdrop application exits.
+        """
+        if self.context is not None:
+            self.pub_socket.send_pyobj('on_app_exit')
+
+    def on_protocol_swapped(self, old_protocol, protocol):
+        """
+        Handler called when a different protocol is swapped in (e.g., when
+        a protocol is loaded or a new protocol is created).
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_protocol_swapped',
+                                        'data': {'old_protocol': old_protocol,
+                                                 'protocol': protocol}})
+
+    def on_protocol_changed(self):
+        """
+        Handler called when a protocol is modified.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_protocol_changed',
+                                        'data': {}})
+
+    def on_protocol_run(self):
+        """
+        Handler called when a protocol starts running.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_protocol_run',
+                                        'data': {}})
+
+    def on_protocol_pause(self):
+        """
+        Handler called when a protocol is paused.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_protocol_pause',
+                                        'data': {}})
+
+    def on_dmf_device_swapped(self, old_dmf_device, dmf_device):
+        """
+        Handler called when a different DMF device is swapped in (e.g., when
+        a new device is loaded).
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_protocol_swapped',
+                                        'data': {'old_dmf_device':
+                                                 old_dmf_device,
+                                                 'dmf_device': dmf_device}})
+
+    def on_dmf_device_changed(self):
+        """
+        Handler called when a DMF device is modified (e.g., channel
+        assignment, scaling, etc.). This signal is also sent when a new
+        device is imported or loaded from outside of the main device
+        directory.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_dmf_device_changed',
+                                        'data': {}})
+
+    def on_experiment_log_changed(self, experiment_log):
+        """
+        Handler called when the current experiment log changes (e.g., when a
+        protocol finishes running.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_experiment_log_changed',
+                                        'data': {'experiment_log':
+                                                 experiment_log}})
+
+    def on_experiment_log_selection_changed(self, data):
+        """
+        Handler called whenever the experiment log selection changes.
+
+        Parameters:
+            data : experiment log data (list of dictionaries, one per step)
+                for the selected steps
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal':
+                                        'on_experiment_log_selection_changed',
+                                        'data': {'data': data}})
+
+    def on_app_options_changed(self, plugin_name):
+        """
+        Handler called when the app options are changed for a particular
+        plugin.  This will, for example, allow for GUI elements to be
+        updated.
+
+        Parameters:
+            plugin : plugin name for which the app options changed
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_app_options_changed',
+                                        'data': {'plugin_name': plugin_name}})
+
+    def on_step_options_changed(self, plugin, step_number):
+        """
+        Handler called when the step options are changed for a particular
+        plugin.  This will, for example, allow for GUI elements to be
+        updated based on step specified.
+
+        Parameters:
+            plugin : plugin instance for which the step options changed
+            step_number : step number that the options changed for
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_step_options_changed',
+                                        'data': {'plugin': plugin,
+                                                 'step_number': step_number}})
+
+    def on_step_options_swapped(self, plugin, old_step_number, step_number):
+        """
+        Handler called when the step options are changed for a particular
+        plugin.  This will, for example, allow for GUI elements to be
+        updated based on step specified.
+
+        Parameters:
+            plugin : plugin instance for which the step options changed
+            step_number : step number that the options changed for
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_step_options_swapped',
+                                        'data': {'plugin': plugin,
+                                                 'old_step_number':
+                                                 old_step_number,
+                                                 'step_number': step_number}})
+
+    def on_step_swapped(self, old_step_number, step_number):
+        """
+        Handler called when the current step is swapped.
+        """
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_step_swapped',
+                                        'data': {'old_step_number':
+                                                 old_step_number,
+                                                 'step_number': step_number}})
 
     def on_step_run(self):
         """
@@ -101,94 +259,26 @@ class DropletPlanningPlugin(Plugin, AppDataController, StepOptionsController):
             'Repeat' - repeat the step
             or 'Fail' - unrecoverable error (stop the protocol)
         """
-        app = get_app()
-        logger.info('[DropletPlanningPlugin] on_step_run(): step #%d',
-                    app.protocol.current_step_number)
-        app_values = self.get_app_values()
-        device_step_options = app.dmf_device_controller.get_step_options()
-        try:
-            if self.timeout_id is not None:
-                # Timer was already set, so cancel previous timer.
-                gobject.source_remove(self.timeout_id)
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_step_run', 'data': {}})
 
-            drop_route_groups = (device_step_options.drop_routes
-                                 .groupby('route_i'))
-            # Look up the drop routes for the current step.
-            self.step_drop_routes = OrderedDict([(route_i, df_route_i)
-                                                 for route_i, df_route_i in
-                                                 drop_route_groups])
-            # Get the number of transitions in each drop route.
-            self.step_drop_route_lengths = drop_route_groups['route_i'].count()
-            self.transition_counter = 0
-            self.start_time = datetime.now()
-            gobject.idle_add(self.on_timer_tick, False)
-            self.timeout_id = gobject.timeout_add(app_values
-                                                  ['transition_duration_ms'],
-                                                  self.on_timer_tick)
-        except:
-            print "Exception in user code:"
-            print '-'*60
-            traceback.print_exc(file=sys.stdout)
-            print '-'*60
-            # An error occurred while initializing Analyst remote control.
-            emit_signal('on_step_complete', [self.name, 'Fail'])
-
-    def on_timer_tick(self, continue_=True):
-        app = get_app()
-        try:
-            if self.transition_counter < self.step_drop_route_lengths.max():
-                active_step_lengths = (self.step_drop_route_lengths
-                                       .loc[self.step_drop_route_lengths >
-                                            self.transition_counter])
-                device_view = app.dmf_device_controller.view
-                for route_i, length_i in active_step_lengths.iteritems():
-                    # Remove custom coloring for previously active electrode.
-                    if self.transition_counter > 0:
-                        transition_i = (self.step_drop_routes[route_i]
-                                        .iloc[self.transition_counter - 1])
-                        device_view.set_electrode_color_by_index(transition_i
-                                                                 .electrode_i)
-                    # Add custom coloring to active electrode.
-                    transition_i = (self.step_drop_routes[route_i]
-                                    .iloc[self.transition_counter])
-                    device_view.set_electrode_color_by_index(transition_i
-                                                             .electrode_i,
-                                                             (255, 255, 255))
-                gtk.idle_add(app.dmf_device_controller.view.update_draw_queue)
-                self.transition_counter += 1
-            else:
-                emit_signal('on_step_complete', [self.name, None])
-                self.timeout_id = None
-                self.start_time = None
-                self.transition_counter = 0
-                return False
-        except:
-            print "Exception in user code:"
-            print '-'*60
-            traceback.print_exc(file=sys.stdout)
-            print '-'*60
-            emit_signal('on_step_complete', [self.name, 'Fail'])
-            self.timeout_id = None
-            self.remote = None
-            return False
-        return continue_
-
-    def on_step_options_swapped(self, plugin, old_step_number, step_number):
+    def on_step_complete(self, plugin_name, return_value=None):
         """
-        Handler called when the step options are changed for a particular
-        plugin.  This will, for example, allow for GUI elements to be
-        updated based on step specified.
+        Handler called whenever a plugin completes a step.
 
-        Parameters:
-            plugin : plugin instance for which the step options changed
-            step_number : step number that the options changed for
+        return_value can be one of:
+            None
+            'Repeat' - repeat the step
+            or 'Fail' - unrecoverable error (stop the protocol)
         """
-        pass
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_experiment_log_changed',
+                                        'data': {}})
 
-    def on_step_swapped(self, old_step_number, step_number):
-        """
-        Handler called when the current step is swapped.
-        """
+    def on_step_created(self, step_number):
+        if self.verify_connected():
+            self.pub_socket.send_pyobj({'signal': 'on_step_created',
+                                        'data': {'step_number': step_number}})
 
 
 PluginGlobals.pop_env()
