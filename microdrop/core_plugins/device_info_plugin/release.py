ca90c3012d728dc227d7f3d8562a9490867e28a4
Christian Fobel <christian@fobel.net>
2015-12-14 08:27:19 -0500

    Fork to create monitor_plugin

--- microdrop/core_plugins/device_info_plugin/release.py
+++ microdrop/core_plugins/device_info_plugin/release.py
@@ -4,8 +4,8 @@ import yaml
 from microdrop_utility import Version
 from path_helpers import path
 
-package_name = 'droplet_planning_plugin'
-plugin_name = 'wheelerlab.droplet_planning_plugin'
+package_name = 'monitor_plugin'
+plugin_name = 'wheelerlab.monitor_plugin'
 
 # create a version sting based on the git revision/branch
 version = str(Version.from_git_repository())
