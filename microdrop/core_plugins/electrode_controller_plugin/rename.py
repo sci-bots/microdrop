4285978ef12ba523b4e6dd669f776872b1bcaa56
Christian Fobel <christian@fobel.net>
2015-12-19 18:20:21 -0500

    [WIP] Fork to start device_info_plugin

--- microdrop/core_plugins/electrode_controller_plugin/rename.py
+++ microdrop/core_plugins/electrode_controller_plugin/rename.py
@@ -65,4 +65,4 @@ def parse_args(args=None):
 
 if __name__ == '__main__':
     args = parse_args()
-    main('.', 'zmq-hub-plugin', args.new_name)
+    main('.', 'device-info-plugin', args.new_name)
