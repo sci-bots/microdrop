b290cb5ee75cd5c6ebf34525e5358ac8d662679c
Christian Fobel <christian@fobel.net>
2016-03-17 14:18:09 -0400

    Don't require unneeded pyutilib modules

--- pavement.py
+++ pavement.py
@@ -18,9 +18,11 @@ install_requires = ['application_repository>=0.5', 'blinker', 'configobj',
                     'networkx', 'pandas', 'path-helpers>=0.2', 'paver>=1.2.4',
                     'pip-helpers>=0.5', 'pygst-utils>=0.3.post3',
                     'pygtk_textbuffer_with_undo', 'pyparsing',
-                    'pyutilib==3.9.2706', 'pyyaml', 'pyzmq', 'run-exe>=0.5',
-                    'si-prefix', 'scipy', 'svgwrite', 'task_scheduler',
-                    'tornado', 'wheeler.pygtkhelpers>=0.12.post7']
+                    'pyutilib.component.core>=4.4.1',
+                    'pyutilib.component.loader>=3.3.1', 'pyyaml', 'pyzmq',
+                    'run-exe>=0.5', 'si-prefix', 'scipy', 'svgwrite',
+                    'task_scheduler', 'tornado',
+                    'wheeler.pygtkhelpers>=0.12.post7']
 
 if platform.system() == 'Windows':
     install_requires += ['matplotlib>=1.5.0', 'pycairo-gtk2-win', 'pygtk2-win',
