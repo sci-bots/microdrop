7377bca3978a2bdbe0dae0d5b6e49e95ba71b1b5
lucaszw <lucas.zeer@gmail.com>
2017-07-19 15:55:41 -0400

    exit with errorcode 1 if nosetests fails

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -14,6 +14,7 @@ REM Install source directory as Python package.
 "%PYTHON%" -m pip install --no-cache --find-links http://192.99.4.95/wheels --trusted-host 192.99.4.95 .
 REM TODO: Find way to get source directory path from within appveyor
 nosetests "%SRC_DIR%" -vv --with-xunit
+if errorlevel 1 exit 1
 
 REM If a project directory specified, then copy results into the directory
 IF DEFINED PROJECT_DIRECTORY (
