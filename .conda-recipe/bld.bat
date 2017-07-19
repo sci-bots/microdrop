e24bdbf29bfe3f7b6d68b8af0d5b3dd884c5df4c
lucaszw <lucas.zeer@gmail.com>
2017-07-19 13:13:05 -0400

    add nosetests to end of conda build

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -12,4 +12,12 @@ REM an `if` block to only execute during package installation.
 
 REM Install source directory as Python package.
 "%PYTHON%" -m pip install --no-cache --find-links http://192.99.4.95/wheels --trusted-host 192.99.4.95 .
+REM TODO: Find way to get source directory path from within appveyor
+nosetests "%SRC_DIR%" -vv --with-xunit
+
+REM If a project directory specified, then copy results into the directory
+IF DEFINED PROJECT_DIRECTORY (
+  cp .\\nosetests.xml "%PROJECT_DIRECTORY%"\\nosetests.xml
+  dir "%PROJECT_DIRECTORY%"
+)
 if errorlevel 1 exit 1
