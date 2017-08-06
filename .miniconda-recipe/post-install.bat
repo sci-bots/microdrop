0f9038af66885a6752d13155f969fa1fc1e1703d
Ryan Fobel <ryan@fobel.net>
2017-08-05 20:59:07 -0400

    Add user-prompt and step-label plugins

--- .miniconda-recipe/post-install.bat
+++ .miniconda-recipe/post-install.bat
@@ -1,2 +1,2 @@
-call Scripts\activate.bat & python -c "import logging; logging.basicConfig(level=logging.DEBUG); import mpm.api; mpm.api.enable_plugin('droplet_planning_plugin'); mpm.api.enable_plugin('dmf_device_ui_plugin'); mpm.api.enable_plugin('dropbot_plugin')"
+call Scripts\activate.bat & python -c "import logging; logging.basicConfig(level=logging.DEBUG); import mpm.api; mpm.api.enable_plugin('droplet_planning_plugin'); mpm.api.enable_plugin('dmf_device_ui_plugin'); mpm.api.enable_plugin('dropbot_plugin');mpm.api.enable_plugin('user_prompt_plugin'); mpm.api.enable_plugin('step_label_plugin')"
 if errorlevel 1 exit 1
