e8467fefbfa8c653dd9352a14f2be318f6a20992
Christian Fobel <christian@fobel.net>
2017-08-18 21:41:16 -0400

    Save extracted module names for enabled plugins
    Since v2.10.3, Conda package names for enabled plugins were recorded to
    the `microdrop.ini` file instead of the corresponding plugin Python module
    names.  This caused an issue when attempting to import the plugins in the
    saved list of enabled MicroDrop plugins, since the imports should
    reference the Python module and **not** the Conda package name.
    
    As of this commit, the plugin module names for enabled plugins are
    extracted from the corresponding Conda package names and saved to the
    `microdrop.ini` configuration file.

--- microdrop/gui/plugin_manager_dialog.py
+++ microdrop/gui/plugin_manager_dialog.py
@@ -87,12 +87,19 @@ class PluginManagerDialog(object):
         self.window.hide()
         for p in self.controller.plugins:
             package_name = p.get_plugin_info().package_name
+            # Extract importable Python module name from Conda package name.
+            #
+            # XXX Plugins are currently Python modules, which means that the
+            # installed plugin directory must be a valid module name. However,
+            # Conda package name conventions may include `.` and `-`
+            # characters.
+            module_name = package_name.split('.')[-1].replace('-', '_')
             if p.enabled():
-                if package_name not in app.config["plugins"]["enabled"]:
-                    app.config["plugins"]["enabled"].append(package_name)
+                if module_name not in app.config["plugins"]["enabled"]:
+                    app.config["plugins"]["enabled"].append(module_name)
             else:
-                if package_name in app.config["plugins"]["enabled"]:
-                    app.config["plugins"]["enabled"].remove(package_name)
+                if module_name in app.config["plugins"]["enabled"]:
+                    app.config["plugins"]["enabled"].remove(module_name)
         app.config.save()
         if self.controller.restart_required:
             logger.warning('\n'.join(['Plugins and/or dependencies were '
