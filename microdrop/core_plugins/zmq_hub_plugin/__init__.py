28b5259b01feba7a1b476fbba316ab9f3d6d0bd2
Christian Fobel <christian@fobel.net>
2017-09-11 14:00:11 -0400

    Remove GPL copyright notices

--- microdrop/core_plugins/zmq_hub_plugin/__init__.py
+++ microdrop/core_plugins/zmq_hub_plugin/__init__.py
@@ -1,21 +1,3 @@
-"""
-Copyright 2015 Christian Fobel
-
-This file is part of zmq_hub_plugin.
-
-zmq_hub_plugin is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-dmf_control_board is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with zmq_hub_plugin.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from multiprocessing import Process
 import logging
 
