4c213f4991e72221e159a77f968fb8bb28b8997b
Christian Fobel <christian@fobel.net>
2018-01-18 13:42:54 -0500

    fix(installer): only fix noarch if needed

--- .miniconda-recipe/post-install.bat
+++ .miniconda-recipe/post-install.bat
@@ -3,11 +3,13 @@ REM Explicitly move noarch packages into `Lib/site-packages` as a workaround to
 REM [this issue][i86] with lack of `constructor` support for `noarch` packages.
 REM
 REM [i86]: https://github.com/conda/constructor/issues/86#issuecomment-330863531
+IF EXIST site-packages (
 for /D %%i in (site-packages/*) do IF "%%~xi" == "" (IF NOT EXIST Lib\site-packages\%%i (
     echo Move noarch package: %%i
     move site-packages\%%i Lib\site-packages
 ))
 rmdir /S/Q site-packages
+)
 
 @echo on
 REM Link installed plugins into Conda MicroDrop activated plugins directory.
