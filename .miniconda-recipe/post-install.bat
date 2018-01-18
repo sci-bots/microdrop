860425f25ff0264793d3dcd493237ec9b3340ace
Christian Fobel <christian@fobel.net>
2018-01-18 13:43:33 -0500

    fix(installer): fix post-install Conda activate

--- .miniconda-recipe/post-install.bat
+++ .miniconda-recipe/post-install.bat
@@ -13,11 +13,11 @@ rmdir /S/Q site-packages
 
 @echo on
 REM Link installed plugins into Conda MicroDrop activated plugins directory.
-call Scripts\activate.bat & python -m mpm.bin.api enable droplet_planning_plugin dmf_device_ui_plugin dropbot_plugin user_prompt_plugin step_label_plugin
+call Scripts\activate.bat %CD% & python -m mpm.bin.api enable droplet_planning_plugin dmf_device_ui_plugin dropbot_plugin user_prompt_plugin step_label_plugin
 
 REM Load plugins by default
-call Scripts\activate.bat & microdrop-config edit --append plugins.enabled droplet_planning_plugin
-call Scripts\activate.bat & microdrop-config edit --append plugins.enabled dmf_device_ui_plugin
-call Scripts\activate.bat & microdrop-config edit --append plugins.enabled dropbot_plugin
-call Scripts\activate.bat & microdrop-config edit --append plugins.enabled user_prompt_plugin
-call Scripts\activate.bat & microdrop-config edit --append plugins.enabled step_label_plugin
+call Scripts\activate.bat %CD% & microdrop-config edit --append plugins.enabled droplet_planning_plugin
+call Scripts\activate.bat %CD% & microdrop-config edit --append plugins.enabled dmf_device_ui_plugin
+call Scripts\activate.bat %CD% & microdrop-config edit --append plugins.enabled dropbot_plugin
+call Scripts\activate.bat %CD% & microdrop-config edit --append plugins.enabled user_prompt_plugin
+call Scripts\activate.bat %CD% & microdrop-config edit --append plugins.enabled step_label_plugin
