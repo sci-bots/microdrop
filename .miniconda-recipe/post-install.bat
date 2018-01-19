d2dd2ae8096d4ed67208e71e39218037108a1fbf
Christian Fobel <christian@fobel.net>
2018-01-19 02:27:26 -0500

    fix(installer): also move file noarch packages
    Prior to this commit, only noarch packages containing directory modules
    were explicitly moved into `Lib/site-packages` as a workaround to
    [this issue][i86] (lack of `constructor` support for `noarch` packages).
    
    As of this commit, noarch packages containing file modules (i.e., `.py`
    files) are also moved.
    
    [i86]: https://github.com/conda/constructor/issues/86#issuecomment-330863531

--- .miniconda-recipe/post-install.bat
+++ .miniconda-recipe/post-install.bat
@@ -4,7 +4,13 @@ REM [this issue][i86] with lack of `constructor` support for `noarch` packages.
 REM
 REM [i86]: https://github.com/conda/constructor/issues/86#issuecomment-330863531
 IF EXIST site-packages (
+REM Move directory modules.
 for /D %%i in (site-packages/*) do IF "%%~xi" == "" (IF NOT EXIST Lib\site-packages\%%i (
+    echo Move noarch package: %%i
+    move site-packages\%%i Lib\site-packages
+))
+REM Move file modules.
+for %%i in (site-packages/*.py) do IF "%%~xi" == ".py" (IF NOT EXIST Lib\site-packages\%%i (
     echo Move noarch package: %%i
     move site-packages\%%i Lib\site-packages
 ))
