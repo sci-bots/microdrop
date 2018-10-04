6617d489d6240dc4b4ed11f5e5dd252cf1a83b1b
Christian Fobel <christian@fobel.net>
2018-10-04 17:09:41 -0400

    feat(config): add update_recent() function
    Add `update_recent()` helper function to `default_paths` module, which
    updates a list of recently accessed filenames in the specified
    configuration section given the path of an accessed file.

--- microdrop/default_paths.py
+++ microdrop/default_paths.py
@@ -1,6 +1,7 @@
 '''
 .. versionadded:: X.X.X
 '''
+import json
 import platform
 
 from logging_helpers import _L
@@ -17,3 +18,32 @@ for dir_i in (DEVICES_DIR, PROTOCOLS_DIR):
         _L().debug('Create default directory `%s` since it does not exist.',
                    dir_i)
         dir_i.makedirs_p()
+
+
+def update_recent(key, config, filename, n=5):
+    '''
+    Update ``'recent'`` list in specified config section with filename.
+
+    Parameters
+    ----------
+    key : str
+        Configuration section name (e.g., ``"protocol"``).
+    config : microdrop.config.Config
+    filename : str
+        File path to insert/update in recent list.
+    n : int, optional
+        Maximum number of items to store in recent list.
+
+    Returns
+    -------
+    list[str]
+        List of recent locations.
+    '''
+    recent_locations = json.loads(config[key].get('recent', '[]'))
+    if filename in recent_locations:
+        recent_locations.remove(filename)
+    recent_locations.insert(0, filename)
+    recent_locations = recent_locations[:n]
+    config[key]['recent'] = json.dumps(recent_locations)
+    config.save()
+    return recent_locations
